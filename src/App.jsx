0101854092a139e445543dadd3659eadc5a8aa1c
Hifza Kanwal <142047631+hifza-kanwal@users.noreply.github.com>
2023-10-06 16:14:06 +0000

    omair

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Hifza kanwal dev",
+  name: "Omair ",
   title: "FLutter dev",
   email: "alex@example.com",
   gitHub: "microsoft",
